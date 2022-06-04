0e880c7c22871094adb6bc03c52caedbcdfc1d65
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-06-04 17:22:52 +0200

    add Model Category and Post done

--- app/Post.php
+++ app/Post.php
@@ -7,5 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    protected $guarded = [];
     protected $fillable = ['title', 'content', 'slug'];
+
+    public static function generateSlug($title)
+    {
+        $slug = Str::slug($title);
+        $alternativeSlug = $slug;
+        $postFound = Post::where("slug", $slug)->first();
+        $counter = 1;
+        while ($postFound) {
+            $alternativeSlug = $slug . "_" . $counter;
+            $counter++;
+            $postFound = Post::where("slug", $alternativeSlug)->first();
+        }
+        return $alternativeSlug;
+    }
+
+    public function category()
+    {
+        return $this->belongsTo("App\Category");
+    }
 }
