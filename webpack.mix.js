7ea066586f0565a378ad5059e7f91124af080999
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-06-08 16:50:46 +0200

    create front-app.js and add front-app to the mix-manifest and webpack-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,9 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").sass(
-    "resources/sass/app.scss",
-    "public/css"
-);
+mix.js("resources/js/app.js", "public/js")
+    .js("resources/js/front-app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css");
 
 mix.copyDirectory(
     "node_modules/@fortawesome/fontawesome-free/webfonts",
