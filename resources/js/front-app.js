450ea68bdf0f661fff65195d7e2dd33e866fc384
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-06-08 17:25:59 +0200

    called axios

--- resources/js/front-app.js
+++ resources/js/front-app.js
@@ -6,6 +6,16 @@
 
 require("./bootstrap");
 
+window.axios = require("axios");
+axios
+    .get("http://127.0.0.1:8000/api/posts")
+    .then((results) => {
+        console.log("results");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
 window.Vue = require("vue");
 
 /**
